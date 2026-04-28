f884dd3c0611f79a0014009f5bf4669ee167b596
touhami-mouhamed <inquiredz@gmail.com>
2026-04-28 01:34:26 +0100

    add more logic

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 
-const random_number = Math.floor(Math.random() * 21);
+let random_number = Math.floor(Math.random() * 21);
 
 console.log(random_number);
 
 let hint = document.querySelector('.hint')
 let number_input = document.getElementById('field');
 let button = document.querySelector('.btn');
+let current_score = 20;
+let highest_score = 0;
+let score_text = document.getElementById('score');
+let highest_text = document.getElementById('highest');
+score_text.innerHTML = ` ${current_score}`
+highest_text.innerHTML = `${highest_score}`
+
+
 button.addEventListener("click", () => {
-
-    if (number_input.value < random_number) {
+    let guess = Number(number_input.value);
+    if (guess < random_number) {
         hint.textContent = "higher";
-    } else if (number_input.value > random_number) {
-        hint.textContent = "lower";
-    } else {
-        hint.textContent = `congrats the number is ${random_number}`;
-    }
+        current_score--;
+        score_text.innerHTML = ` ${current_score}`
 
+
+    } else if (guess > random_number) {
+
+        hint.textContent = "lower";
+        current_score--;
+        score_text.innerHTML = ` ${current_score}`
+    } else {
+        highest_score = Math.max(highest_score, current_score);
+        hint.textContent = `congrats the number is ${random_number}`;
+        highest_text.innerHTML = `${highest_score}`
+
+    }
+    if (current_score <= 0) {
+        hint.textContent = "YOU LOSE Try Again";
+        button.disabled = true;
+    }
+})
+
+
+let reset = document.getElementById('reset');
+
+reset.addEventListener("click", () => {
+    random_number = Math.floor(Math.random() * 21);
+    console.log(random_number);
+    hint.textContent = "start guessing ...";
+    current_score = 20;
+    score_text.innerHTML = ` ${current_score}`
+    button.disabled = false;
+    number_input.value = ""
 })
 
